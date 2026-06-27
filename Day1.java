f5d132c677031a7672a822d57270d569ac09de56
Vasudha Vasudev <vasudhavasuudev18@gmail.com>
2026-06-27 23:15:11 +0530

    Day 1: Array insertion in Java

---
+++ Day1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter position: ");
+        int pos = sc.nextInt();
+
+        System.out.print("Enter element to insert: ");
+        int x = sc.nextInt();
+
+        for (int i = n; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[pos - 1] = x;
+
+        System.out.println("\nUpdated Array:");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+#100 Days of DSA in Java
+
+Welcome to my 100 Days of DSA journey!
+
+## About
+This repository contains my daily Java solutions for the 100 Days of DSA Challenge.
+
+## Goals
+- Solve DSA problems consistently
+- Improve Java programming
+- Prepare for internships and placements
+
+## Progress
+
+| Day | Topic | Status |
+|-----|-------|--------|
+| Day 1 | Array Insertion | ✅ Completed |
