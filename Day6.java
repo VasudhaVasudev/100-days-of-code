c51721dcf2d1663c2e682f93a4d54d280fe6f7db
Vasudha Vasudev <vasudhavasuudev18@gmail.com>
2026-07-02 21:30:00 +0530

    Day6- Remove Duplicates from Sorted Array

---
+++ Day6.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " sorted elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (n == 0) {
+            System.out.println("Array is empty.");
+            return;
+        }
+
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+
+        }
+
+        System.out.println("\nUnique Elements:");
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
